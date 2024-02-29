cf584b6a8604837e8de849813ea9a39c91056e69
Muneyy <muneyyyy@gmail.com>
2024-02-29 10:32:23 +0800

    feat: add overlay and close sidebar on outside click

--- src/components/Header/Sidebar.tsx
+++ src/components/Header/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import styles from './Sidebar.module.sass'
 import CloseIcon from '@/svgs/CloseIcon'
 import Link from 'next/link'
@@ -14,25 +14,49 @@ const Sidebar = ({
     setIsMenuOpen(false)
   }
 
+  const sidebarRef = useRef<HTMLDivElement>(null)
+  const overlayRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      e.stopPropagation()
+      const overlayClicked = overlayRef.current!.contains(e.target as Node)
+      const sidebarClicked = sidebarRef.current!.contains(e.target as Node)
+
+      if (overlayClicked && !sidebarClicked) {
+        setIsMenuOpen(false)
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [])
+
   return (
-    <aside className={styles.sidebar}>
-      <button
-        type="button"
-        aria-label="close sidebar menu"
-        onClick={handleCloseClick}
-      >
-        <CloseIcon />
-      </button>
-      <ul>
-        {NAV_LINKS.map((link) => (
-          <li key={link}>
-            <Link href={link === 'Home' ? '/' : `/${link.toLowerCase()}`}>
-              {link}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </aside>
+    <>
+      <div ref={overlayRef} className={styles.overlay} />
+      <aside ref={sidebarRef} className={styles.sidebar}>
+        <button
+          type="button"
+          aria-label="close sidebar menu"
+          onClick={handleCloseClick}
+        >
+          <CloseIcon />
+        </button>
+        <ul>
+          {NAV_LINKS.map((link) => (
+            <li key={link}>
+              <Link href={link === 'Home' ? '/' : `/${link.toLowerCase()}`}>
+                {link}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </aside>
+    </>
   )
 }
 
