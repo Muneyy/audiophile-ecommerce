71d240602c4372841e9db170bbc6c5db9e6d95fd
Muneyy <muneyyyy@gmail.com>
2024-02-29 18:17:44 +0800

    fix: remove commented code

--- src/lib/contentful.js
+++ src/lib/contentful.js
@@ -5,17 +5,3 @@ export const client = contentful.createClient({
   environment: 'master', // defaults to 'master' if not set
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
 })
-
-// client
-//   .getEntry('23ajyJGN6FZHErhMhAjYLu')
-//   .then((entry: any) => console.log(entry))
-//   .catch(console.error)
-
-// sample code for SSR
-// export async function getData() {
-//   const data = await client
-//     .getEntry('23ajyJGN6FZHErhMhAjYLu')
-//     .then((entry: any) => entry)
-//     .catch(console.error)
-//   return data
-// }
