5a7967517ccb0dbe006049789d5f77d5f2b105fb
Muneyy <muneyyyy@gmail.com>
2024-03-04 17:02:18 +0800

    temp feat: working contentful graphql fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,41 @@
+import fetchGql from '@/lib/fetchGql'
 import styles from './page.module.sass'
+import { notFound } from 'next/navigation'
+
+async function getData() {
+  const query = `
+    {
+      productCollection(where: { title_contains: "Headphones" }) {
+        items {
+          title
+          description
+        }
+      }
+    }
+  `
+
+  try {
+    const response = await fetchGql(query)
+    return response.data
+  } catch (error: unknown) {
+    // throw new Error(`Error: ${error}`)
+    console.error(error)
+  }
+}
 
 export default async function Home() {
-  return <main className={styles.main}></main>
+  const data = await getData()
+
+  if (!data) {
+    return notFound()
+  }
+
+  console.log(data)
+  if (data) {
+    return (
+      <main className={styles.main}>
+        <p>{data.productCollection.items[0].title}</p>
+      </main>
+    )
+  }
 }
