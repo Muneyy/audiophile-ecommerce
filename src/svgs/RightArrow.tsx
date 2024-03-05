7e0d26612f43766cb11a5dd967866fc4ca5bbc0b
Muneyy <muneyyyy@gmail.com>
2024-03-05 16:58:44 +0800

    feat: add categories section
    bug: displays over sidebar

---
+++ src/svgs/RightArrow.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const RightArrow = () => {
+  return (
+    // <!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+    <svg
+      width="25px"
+      height="25px"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M10 7L15 12L10 17"
+        stroke="#000000"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
+
+export default RightArrow
