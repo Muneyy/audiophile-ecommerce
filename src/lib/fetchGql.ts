d76bc4d2c7215e9e5191de6b474758942b0f93b6
Muneyy <muneyyyy@gmail.com>
2024-03-06 16:58:58 +0800

    chore: decomposition of homepage and remove comments

--- src/lib/fetchGql.ts
+++ src/lib/fetchGql.ts
@@ -1,11 +1,3 @@
-// const contentful = require('contentful')
-
-// export const client = contentful.createClient({
-//   space: process.env.CONTENTFUL_SPACE_ID,
-//   environment: 'master', // defaults to 'master' if not set
-//   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-// })
-
 export default async function fetchGql(query: string) {
   try {
     const response = await fetch(
