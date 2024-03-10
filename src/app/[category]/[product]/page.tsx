1f9b47715c2fe76bcceb44ee4b1dbd5a0f4634cf
Muneyy <muneyyyy@gmail.com>
2024-03-10 16:03:59 +0800

    feat: image gallery section

--- src/app/[category]/[product]/page.tsx
+++ src/app/[category]/[product]/page.tsx
@@ -104,6 +104,32 @@ const Page = () => {
           ))}
         </div>
       </section>
+      <section className={styles.imageGallery}>
+        <div className={styles.columnOne}>
+          {fetchedData.imageGallery.slice(0, 2).map((image, index) => (
+            <div key={index} className={styles.imageGalleryContainer}>
+              <Image
+                src={image}
+                alt={title}
+                width={0}
+                height={0}
+                sizes="100vw"
+              />
+            </div>
+          ))}
+        </div>
+        <div className={styles.columnTwo}>
+          <div className={styles.imageGalleryContainer}>
+            <Image
+              src={fetchedData.imageGallery[2]}
+              alt={title}
+              width={0}
+              height={0}
+              sizes="100vw"
+            />
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
