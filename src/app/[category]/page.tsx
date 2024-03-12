0a12a01624d13723170d68011c5bc91fe0081562
Ivan Martinez <107381315+Muneyy@users.noreply.github.com>
2024-03-13 07:20:21 +0800

    Feature/plp (#6)
    * Feature/header (#2)
    
    chore: comment out fetch from contentful
    
    fix: remove commented code
    
    feat: header layout including svgs
    
    feat: sidebar layout
    
    feat: add overlay and close sidebar on outside click
    
    fix: remove commented code
    
    * Previous commits from main
    
    Initial setup
    
    feat: add eslint-config-prettier and eslint-plugin-jsx-a11y
    
    setup: add prettier
    
    setup: add husky and lint-staged; also add eslint rule to use single quotes
    
    feat: fetch entry from contentful
    
    Merge branch 'main' of https://github.com/Muneyy/audiophile-ecommerce
    
    * Feature/homepage (#4)
    
    feat: responsive hero banner with local assets
    
    feat: add categories section
    
    bug: displays over sidebar
    
    Fix: add z-index for sidebar and overlay
    
    feat: responsiveness for category cards
    
    feat: add hover for see product button
    
    feat: add first featured product card
    
    feat: card two of featured section
    
    feat: add third card for featured section
    
    chore: decomposition of homepage and remove comments
    
    * feat: add back button, image, description, features, and box content
    
    * feat: image gallery section
    
    * feat: add recommendations section
    
    * feat: add back button and minor refactor of imageGallery and imageRecommendations
    
    * feat: add responsiveness and refactor to separate components
    
    * chore: move shared components into separate folder
    
    * feat: add category plp layout
    
    * feat: add dynamic links to buttons
    
    * feat: add working links for homepage buttons and refactor mock fetchedData
    
    * fix: refactor buttons to separate component
    
    * fix: minor alignment issue for PDP image gallery

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import styles from './page.module.sass'
+import ImageAndDescription from '@/components/shared/ImageAndDescription'
+import CategoriesSection from '@/components/shared/CategoriesSection'
+import { fetchedDataForPLP } from '../mockRequests/mockRequests'
 
-const Page = () => {
+type TCategory = {
+  category: 'speakers' | 'earphones' | 'headphones'
+}
+
+const fetchedData = fetchedDataForPLP
+
+const Page = ({ params }: { params: TCategory }) => {
+  const categoryTitle = params.category
+
   return (
-    <div>
-      <p>Page</p>
-    </div>
+    <main className={styles.main}>
+      <h1>{categoryTitle}</h1>
+      <div className={styles.productsContainer}>
+        {fetchedData.data.map((item, index) => (
+          <ImageAndDescription
+            key={index}
+            title={item.title}
+            description={item.description}
+            imageProduct={item.imageSrc}
+            price={100}
+            forProductListPage={true}
+            params={{ category: categoryTitle }}
+          />
+        ))}
+      </div>
+      <CategoriesSection />
+    </main>
   )
 }
 
