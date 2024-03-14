ee93cb350073359451b544dc7363339beff44b30
Muneyy <muneyyyy@gmail.com>
2024-03-14 19:01:45 +0800

    feat: contentful fetch for FeaturedSection

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -1,14 +1,88 @@
 import React from 'react'
-import bgBannerDesktop from '@/assets/image-hero.jpg'
-import bgBannerMobile from '@/assets/image-header.jpg'
 import bgFeaturedTwoDesktop from '@/assets/zx7-desktop.jpg'
 import bgFeaturedTwoMobile from '@/assets/zx7-mobile.jpg'
 import BannerSection from './subcomponents/BannerSection'
 import CategoriesSection from '../shared/CategoriesSection'
 import FeaturedSection from './subcomponents/FeaturedSection'
 import styles from './Homepage.module.sass'
+import fetchGql from '@/lib/fetchGql'
 
-const Homepage = () => {
+async function getData() {
+  const queryHomepage = `
+  {
+    homepageContent(id: "7f4y9u2wsYCun1pqLjOJT1") {
+      bannerTitle
+      bannerDescription
+      categories
+      categoriesImagesCollection {
+        items {
+          title
+          description
+          url
+        }
+      }
+      promotionProducts
+      bannerImageCollection {
+        items {
+          title
+          description
+          url
+        }
+      }
+    }
+  }
+  `
+  try {
+    const data = await fetchGql(queryHomepage)
+    return data.data
+  } catch (err: unknown) {
+    throw new Error(`Error fetching data: ${err}`)
+  }
+}
+type TypeHomepageContent = {
+  homepageContent: {
+    bannerTitle: string
+    bannerDescription: string
+    categories: string[]
+    categoriesImagesCollection: {
+      items: {
+        title: string
+        description: string
+        url: string
+      }[]
+    }
+    promotionProducts: {
+      items: {
+        title: string
+        description?: string
+        urlDesktop: string
+        urlMobile: string
+      }[]
+    }
+    promotionImagesCollection: {
+      items: {
+        title: string
+        description: string
+        url: string
+      }[]
+    }
+    bannerImageCollection: {
+      items: {
+        title: string
+        description: string
+        url: string
+      }[]
+    }
+  }
+}
+
+const Homepage = async () => {
+  const fetchedData: TypeHomepageContent = await getData()
+  const { bannerImageCollection, promotionProducts } =
+    fetchedData.homepageContent
+  const bgBannerDesktop = bannerImageCollection.items[0].url
+  const bgBannerMobile = bannerImageCollection.items[1].url
+
   const dynamicBackgroundImagesStyle = () => {
     // dynamically change url of background-image of hero banner
     // according to screen width
@@ -16,14 +90,14 @@ const Homepage = () => {
       <style scoped>
         {`
       .banner-background {
-        background-image: url(${bgBannerDesktop.src});
+        background-image: url(${bgBannerDesktop});
       }
       .featured-two-bg {
         background-image: url(${bgFeaturedTwoDesktop.src});
       }
       @media (max-width: 1200px) {
         .banner-background {
-          background-image: url(${bgBannerMobile.src});
+          background-image: url(${bgBannerMobile});
         }
 
       }
@@ -36,6 +110,7 @@ const Homepage = () => {
       </style>
     )
   }
+
   return (
     <main className={styles.main}>
       {dynamicBackgroundImagesStyle()}
@@ -43,7 +118,7 @@ const Homepage = () => {
       <div className={styles.categoriesWidth}>
         <CategoriesSection />
       </div>
-      <FeaturedSection />
+      <FeaturedSection promotionProducts={promotionProducts.items} />
     </main>
   )
 }
--- src/components/Homepage/subcomponents/FeaturedSection.tsx
+++ src/components/Homepage/subcomponents/FeaturedSection.tsx
@@ -2,77 +2,93 @@ import React from 'react'
 import styles from './FeaturedSection.module.sass'
 import Image from 'next/image'
 
-import bgCardThree from '@/assets/image-earphones-yx1.jpg'
-import featuredSpeakers from '@/assets/image-speaker-zx9.png'
 import circlesPattern from '@/svgs/CirclesPattern.svg'
 import RedirectButton from '@/components/shared/RedirectButton'
 
-const FEATURED_TITLE = 'ZX9 SPEAKER'
-const FEATURED_DESC =
-  'Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.'
-
-const FEATURED_TITLE_TWO = 'ZX7 Speaker'
-const FEATURED_TITLE_THREE = 'YX1 Earphones'
-
 const BUTTON_TEXT = 'SEE PRODUCT'
 
-const FeaturedSection = () => {
+interface TypesFeaturedSection {
+  promotionProducts: {
+    title: string
+    description?: string
+    urlDesktop: string
+    urlMobile: string
+  }[]
+}
+
+const FeaturedSection = ({ promotionProducts }: TypesFeaturedSection) => {
+  const [firstProduct, secondProduct, thirdProduct] = promotionProducts
+
   return (
-    <section className={styles.sectionFeatured}>
-      <div
-        style={{ backgroundImage: `url(${circlesPattern.src})` }}
-        className={styles.cardOne}
-      >
-        <div className={styles.imageContainer}>
-          <Image
-            src={featuredSpeakers}
-            alt="featured product"
-            width={0}
-            height={0}
-            sizes="100vw"
-          />
+    <>
+      <style scoped>
+        {`
+        .featured-two-bg {
+          background-image: url(${secondProduct.urlDesktop});
+        }
+        @media (max-width: 768px) {
+          .featured-two-bg {
+            background-image: url(${secondProduct.urlMobile});
+          }
+        }
+      `}
+      </style>
+      <section className={styles.sectionFeatured}>
+        <div
+          style={{ backgroundImage: `url(${circlesPattern.src})` }}
+          className={styles.cardOne}
+        >
+          <div className={styles.imageContainer}>
+            <Image
+              src={firstProduct.urlDesktop}
+              alt="featured product"
+              width={0}
+              height={0}
+              sizes="100vw"
+            />
+          </div>
+          <div className={styles.textContainer}>
+            <h2>{firstProduct.title}</h2>
+            <p>{firstProduct.description}</p>
+            <RedirectButton
+              link={`/speakers/${firstProduct.title}`}
+              text={BUTTON_TEXT}
+              ariaLabel={`go to ${firstProduct.title} product page`}
+            />
+          </div>
         </div>
-        <div className={styles.textContainer}>
-          <h2>{FEATURED_TITLE}</h2>
-          <p>{FEATURED_DESC}</p>
+        <div className={`${styles.cardTwo} featured-two-bg`}>
+          <h2>{secondProduct.title}</h2>
           <RedirectButton
-            link={`/speakers/${FEATURED_TITLE}`}
+            link={`/speakers/${secondProduct.title}`}
             text={BUTTON_TEXT}
-            ariaLabel={`go to ${FEATURED_TITLE} product page`}
+            ariaLabel={`go to ${secondProduct.title} product page`}
           />
         </div>
-      </div>
-      <div className={`${styles.cardTwo} featured-two-bg`}>
-        <h2>{FEATURED_TITLE_TWO}</h2>
-        <RedirectButton
-          link={`/speakers/${FEATURED_TITLE_TWO}`}
-          text={BUTTON_TEXT}
-          ariaLabel={`go to ${FEATURED_TITLE_TWO} product page`}
-        />
-      </div>
-      <div className={styles.cardThree}>
-        <div className={styles.imageContainer}>
-          <Image
-            src={bgCardThree}
-            alt="featured product"
-            width={0}
-            height={0}
-            sizes="100vw"
-            style={{
-              objectFit: 'cover',
-            }}
-          />
+        <div className={styles.cardThree}>
+          <div className={styles.imageContainer}>
+            <Image
+              src={thirdProduct.urlDesktop}
+              alt="featured product"
+              width={0}
+              height={0}
+              sizes="100vw"
+              style={{
+                objectFit: 'cover',
+              }}
+            />
+          </div>
+          <div className={styles.textContainer}>
+            <h2>{thirdProduct.title}</h2>
+            <RedirectButton
+              link={`/earphones/${thirdProduct.title}`}
+              text={BUTTON_TEXT}
+              ariaLabel={`go to ${thirdProduct.title} product page`}
+            />
+          </div>
         </div>
-        <div className={styles.textContainer}>
-          <h2>{FEATURED_TITLE_THREE}</h2>
-          <RedirectButton
-            link={`/earphones/${FEATURED_TITLE_THREE}`}
-            text={BUTTON_TEXT}
-            ariaLabel={`go to ${FEATURED_TITLE_THREE} product page`}
-          />
-        </div>
-      </div>
-    </section>
+      </section>
+    </>
   )
 }
 
